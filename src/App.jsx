ee61da7e91b219f7f4d4ec8359666f8af1c92db5
Sandhya Pandey <p91sandhya@gmail.com>
2024-07-22 10:57:44 +0545

    creating nav

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-slate-900 h-screen">
+      <div className="w-full h-fit">
+        <h1 className="font-semibold text-4xl py-5 text-white uppercase bg-black flex justify-center ">
+          Tic Tac Toe Game
+        </h1>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
